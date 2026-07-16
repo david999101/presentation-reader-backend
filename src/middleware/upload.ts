035e547c1767e1a89af9656155f28660fbba3dac
david999101 <davidkiknavelidze9@gmail.com>
2026-07-16 21:49:40 +0400

    upload folder

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,22 +1,45 @@
+// import multer from "multer";
+// import fs from "fs";
+
+// const tempDir = "uploads/temp";
+
+// if (!fs.existsSync(tempDir)) {
+//     fs.mkdirSync(tempDir, { recursive: true });
+// }
+
+// const storage = multer.diskStorage({
+//     destination(req, file, cb) {
+//         cb(null, tempDir);
+//     },
+
+//     filename(req, file, cb) {
+//         cb(null, Date.now() + "-" + file.originalname);
+//     },
+// });
+
+// export const upload = multer({
+//     storage,
+// });
+
 import multer from "multer";
+import path from "path";
 import fs from "fs";
 
-const tempDir = "uploads/temp";
-
-if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, tempDir);
+    destination: (req, file, cb) => {
+        // process.cwd() აბრუნებს პროექტის მთავარ დირექტორიას Render-ზე
+        const uploadPath = path.join(process.cwd(), "uploads");
+        
+        // თუ საქაღალდე არ არსებობს, იქმნება ფაილის ჩაწერის მომენტში
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath);
     },
-
-    filename(req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
-    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
 });
 
-export const upload = multer({
-    storage,
-});
+export const upload = multer({ storage });
